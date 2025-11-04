87b96db35fb2104155bd8b9f91c8e147fde6b601
katerinacejas <katerinacejas3@gmail.com>
2025-11-04 19:29:52 -0300

    init proyecto con las dependencias para mysql

---
+++ src/main/java/com/cuido/cuido/CuidoApplication.java
@@ -0,0 +1,13 @@
+package com.cuido.cuido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuido/cuido/CuidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuido.cuido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
